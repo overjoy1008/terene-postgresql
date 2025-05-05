3f5dc6dcec977019e0ba5a59238834a2810dc9ba
overjoy1008 <overjoy1008@gmail.com>
2025-05-06 04:28:48 +0900

    cors로 외부 브라우저 권한 허용

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express"
+import cors from "cors"
 // import reservationRoutes from "./routes/reservation.routes.js"
 import holidayRoutes from "./routes/holiday.routes.js"
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }));
 
 // app.use("/api/reservations", reservationRoutes)
 app.use("/api/holidays", holidayRoutes)
