f82c98045739771dec207e3e03df00b6b86a4ef2
overjoy1008 <overjoy1008@gmail.com>
2025-05-19 04:15:23 +0900

    [coupons] 실수로 지워버린 controller의 함수들 복구

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -1,3 +1,14 @@
+import * as service from "../services/coupon.service.js"
+
+export const getAllCoupons = async (req, res) => {
+    try {
+        const data = await service.getCoupons()
+        res.json(data)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
 export const insertCoupon = async (req, res) => {
     try {
         const coupon = req.body
@@ -11,6 +22,16 @@ export const insertCoupon = async (req, res) => {
         await service.adjustPriorities(coupon.priority)
         await service.addCoupon(coupon)
         res.status(201).json({ message: "Coupon inserted" })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+export const deleteCoupon = async (req, res) => {
+    try {
+        const id = req.params.id
+        await service.removeCoupon(id)
+        res.status(200).json({ message: `Deleted coupon ${id}` })
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
