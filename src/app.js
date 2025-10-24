cfbc3cf56065cb52ffe0978c6bd96937409a5854
overjoy1008 <overjoy1008@gmail.com>
2025-10-25 01:51:17 +0900

    app.js에서 v3Routes 참고하도록 수정

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import customerRoutes from "./routes/customer.routes.js"
 import couponRoutes from "./routes/coupon.routes.js"
 import orderRoutes from "./routes/order.routes.js"
 import entitiesRoutes from "./routes/entities.routes.js"
+import v3Routes from "./routes/v3.routes.js"
 
 const app = express()
 app.use(express.json())
@@ -35,5 +36,7 @@ app.use("/api/coupons", couponRoutes)
 app.use("/api/orders", orderRoutes)
 // 정규화된 주문 관련 API (v2)
 app.use("/api/v2", entitiesRoutes)
+// patch_250928 적용 API (v3)
+app.use("/api/v3", v3Routes)
 
 export default app
